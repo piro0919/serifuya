31a423a3daa899c2a27d5fed1ff4a38cffb55c31
kohei_kawamura <piro.haniwa@gmail.com>
2020-11-30 20:52:50 +0900

    fix: add locale to dayjs

--- functions/src/voices/id/index.ts
+++ functions/src/voices/id/index.ts
@@ -1,30 +1,40 @@
-import * as functions from 'firebase-functions';
-import { storage as adminStorage } from 'firebase-admin/lib/storage';
-import * as dayjs from "dayjs"
+import * as functions from "firebase-functions";
+import { storage as adminStorage } from "firebase-admin/lib/storage";
+import * as dayjs from "dayjs";
 
 type voicesParams = {
-  firestore: FirebaseFirestore.Firestore
+  firestore: FirebaseFirestore.Firestore;
   storage: adminStorage.Storage;
-}
+};
 
 type ResBody = {
   downloadUrl: string;
   expires: string;
   name: any;
-}
+};
 
-const id = ({ firestore, storage }: voicesParams) => async ({ method, params: { id: voicesId } }: functions.https.Request, response: functions.Response<ResBody>) => {
+const id = ({ firestore, storage }: voicesParams) => async (
+  { method, params: { id: voicesId } }: functions.https.Request,
+  response: functions.Response<ResBody>
+) => {
   if (method === "GET") {
     const docRef = firestore.collection("voices").doc(voicesId);
     const snapshot = await docRef.get();
-    const name = snapshot.get("name")
-    const expires = dayjs().add(3, "minute").toDate();
-    const signedUrl = await storage.bucket().file(`voices/${name}.mp3`).getSignedUrl({
-      expires,
-      action: "read",
-    })
+    const name = snapshot.get("name");
+    const expires = dayjs().locale("ja").add(3, "minute").toDate();
+    const signedUrl = await storage
+      .bucket()
+      .file(`voices/${name}.mp3`)
+      .getSignedUrl({
+        expires,
+        action: "read",
+      });
 
-    response.send({ name, downloadUrl: signedUrl[0], expires: expires.toString() });
+    response.send({
+      name,
+      downloadUrl: signedUrl[0],
+      expires: expires.toString(),
+    });
   }
 };
 
