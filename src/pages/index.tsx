52d566b07019532d299f3f493b44d6be43125a60
kohei_kawamura <piro.haniwa@gmail.com>
2020-11-30 20:26:06 +0900

    chore: wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,34 @@
-// import React from "react";
-// import { GetServerSideProps, NextPage } from "next";
-// import Layout from "components/templates/Layout";
-// import SerifuList from "components/organisms/SerifuList";
-// import Head from "components/templates/Head";
-// import api from "api";
-
-// type Voice = {
-//   id: string;
-//   name: any;
-// };
-
-// export type PagesProps = {
-//   voices: Voice[];
-// };
-
-// const Pages: NextPage<PagesProps> = ({ voices }) => (
-//   <Layout>
-//     <Head />
-//     <SerifuList voices={voices} />
-//   </Layout>
-// );
-
-// export const getServerSideProps: GetServerSideProps<PagesProps> = async () => {
-//   const { data } = await api.get("/voices");
-//   const voices = data.map(({ id, name }) => ({ id, name }));
-
-//   return {
-//     props: {
-//       voices,
-//     },
-//   };
-// };
-
-// export default Pages;
-
 import React from "react";
 import { GetServerSideProps, NextPage } from "next";
+import Layout from "components/templates/Layout";
+import SerifuList from "components/organisms/SerifuList";
+import Head from "components/templates/Head";
+import axios from "axios";
 
-const Pages: NextPage<any> = (a) => {
-  console.log(a);
-
-  return <div>a</div>;
+type Voice = {
+  id: string;
+  name: any;
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export type PagesProps = {
+  voices: Voice[];
+};
+
+const Pages: NextPage<PagesProps> = ({ voices }) => (
+  <Layout>
+    <Head />
+    <SerifuList voices={voices} />
+  </Layout>
+);
+
+export const getServerSideProps: GetServerSideProps<PagesProps> = async () => {
+  const { data } = await axios.get(`${process.env.BASE_URL}/voices`);
+  const voices = data.map(({ id, name }) => ({ id, name }));
+
   return {
-    props: { hoge: process.env },
+    props: {
+      voices,
+    },
   };
 };
 
